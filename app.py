59c313ca5ae6b154f76e94ae41ddabfa3b945754
Shaaguunz <shaaguunkhadka@gmail.com>
2022-03-06 16:01:38 +0545

    updated

--- app.py
+++ app.py
@@ -2,19 +2,28 @@ from flask import Flask,render_template,request
 import numpy as np
 import pandas as pd
 import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
 import re
 from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
+ 
+from nltk.tokenize import word_tokenize
 import joblib
 
 def preprocess(t):
-    p=PorterStemmer()
-    text = re.sub('[^a-zA-Z]',' ',t)
-    text = text.lower()
-    text = text.split()
-    text = [p.stem(i) for i in text]
-    text = ' '.join(text)
-    return text
+     text=t
+     text = text.lower()
+     text = re.sub('\[.*?\]', '', text) # remove square brackets
+     text = re.sub(r'[^\w\s]','',text) # remove punctuation
+     text = re.sub('\w*\d\w*', '', text) # remove words containing numbers
+     text = re.sub('\n', '', text)
+     stop_words = stopwords.words('english')
+     text=" ".join([word for word in text.split() if word not in stop_words])
+
+     text=word_tokenize(text)
+     
+     
+     return text
 
 tfidf = joblib.load('TFIDF')
 model = joblib.load('model')
@@ -31,8 +40,8 @@ def predict():
     if request.method=='POST':
         message = request.form['text']
         message = preprocess(message)
-        data = [message]
-        vect = tfidf.transform(data).toarray()
+        
+        vect = tfidf.transform(message).toarray()
         prediction = model.predict(vect)
         
 	  
